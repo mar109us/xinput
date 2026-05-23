4237f50bf0cab0199127b32a60095180455e334a
Martinius <mar109us@gmail.com>
2026-05-23 21:03:14 +0200

    init

---
+++ dist/src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+/* let output = document.getElementById("output")
+
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+function test() {
+    console.log("mew");
+}
+//# sourceMappingURL=main.js.map
