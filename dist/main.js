fa95d6b77b6a293f044800780c51a0d1e1426a50
Martinius <mar109us@gmail.com>
2026-05-24 17:47:41 +0200

    abandon repo sincebrowser does not allow reading input if browser is not in focus. will move to electron

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,26 @@
-/* let output = document.getElementById("output")
-
- */
-function test() {
-    console.log("mew");
+const output = document.getElementById("output");
+let gamepadIndex;
+window.addEventListener("gamepadconnected", (event) => {
+    gamepadIndex = event.gamepad.index;
+});
+let myGamepad;
+function gameLoop() {
+    if (gamepadIndex !== undefined) {
+        myGamepad = navigator.getGamepads()[gamepadIndex];
+        output.innerHTML = "";
+        myGamepad.buttons
+            .map((e) => e.pressed)
+            .forEach((isPressed, buttonIndex) => {
+            if (isPressed) {
+                output.innerHTML += `<h1>Button ${buttonIndex} is pressed</h1>`;
+            }
+        });
+    }
+    if (gamepadIndex === undefined) {
+        output.innerHTML = `Controller disconnected`;
+    }
+    requestAnimationFrame(gameLoop);
 }
+requestAnimationFrame(gameLoop);
 export {};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
-/* let output = document.getElementById("output")
+const output = document.getElementById("output") as HTMLElement;
 
- */
+let gamepadIndex: any;
+window.addEventListener("gamepadconnected", (event) => {
+	gamepadIndex = event.gamepad.index;
+});
 
-function test() {
-	console.log("mew");
+let myGamepad: any;
+
+function gameLoop() {
+	if (gamepadIndex !== undefined) {
+		myGamepad = navigator.getGamepads()[gamepadIndex];
+		output.innerHTML = "";
+		myGamepad.buttons
+			.map((e: any) => e.pressed)
+			.forEach((isPressed: any, buttonIndex: any) => {
+				if (isPressed) {
+					output.innerHTML += `<h1>Button ${buttonIndex} is pressed</h1>`;
+				}
+			});
+	}
+	if (gamepadIndex === undefined) {
+		output.innerHTML = `Controller disconnected`;
+	}
+	requestAnimationFrame(gameLoop);
 }
+requestAnimationFrame(gameLoop);
